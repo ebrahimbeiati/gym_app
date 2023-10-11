382ae3f90603cddf306dda247e41fe87c13937c3
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-10-11 19:25:43 +0100

    Banner part

---
+++ src/components/SearchExercises.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography} from '@mui/material'
+
+const SearchExercises = () => {
+  return <div>SearchExercises</div>;
+};
+
+export default SearchExercises;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import Exercises from '../components/Exercises'
+import MainBanner from '../components/MainBanner'
+import SearchExercises from '../components/SearchExercises'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <MainBanner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
   )
 }
 
