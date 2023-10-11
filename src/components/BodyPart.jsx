4e8b7066582fecbf8b78c02566a7cda4e52bfb35
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-10-11 22:45:23 +0100

    exercises part

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import {stack, Typography} from '@mui/material';
+import gym from '../assets/icons/gym.png';
 
-const BodyPart = () => {
+const BodyPart = ( {item,setBodyPart, bodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <stack
+    type="button"
+    alignItems="center"
+    justifyContent="center"
+    className="bodyPart-card"
+    sx={
+      {
+        borderTop: bodyPart === item ? "4px solid #FF2625" : "",
+        backgroundColor: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px"
+      }}
+    onClick={() => {
+      setBodyPart(item);
+      window.scrollTo({top: 1800, left: 100, behavior: "smooth"});
+    }}
+    >
+    <img src="{gym}" alt="dumbbell" 
+        style={{width: '40px', height: '40px'}}
+    />
+    <Typography
+    fontSize="24px"
+    fontWeight="bold"
+    color="#3A1212"
+    textTransform="capitalize"
+    >
+      {item}
+    </Typography>
+
+    </stack>
   )
 }
 
--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import Pagination from '@mui/material';
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
-const Exercises = () => {
+
+const Exercises = ({exercises, setExercises, bodyPart}) => {
   return (
     <div>Exercises</div>
   )
