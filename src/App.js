226957cf6788a2bb8ce4f9b9c7838e5d438183c5
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-10-11 16:38:39 +0100

    creating components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import './App.css'
+import {Routes,Route} from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Footer from './components/Footer'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width='400px'>
+    <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Exercise/:id' element={<ExerciseDetail/>} />
+      </Routes>
+    </Box>
   )
 }
 
